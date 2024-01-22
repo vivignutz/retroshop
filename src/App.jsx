58cc51600e0c13fc443219b40b5946f9f6e1f13b
vivignutz <vgnutzmann@gmail.com>
2024-01-22 20:36:32 +0100

    Added reset.css to projetct, and implemented it to the main file

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import NavbarComponent from "./components/NavbarComponent/Navbar.jsx";
 import Categories from "./components/Categories/Categories.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 //import ProductList from "./components/ProductList.jsx"
-import ContactForm from "./components/ContactForm/Contact.jsx";
 import './App.css';
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import './styles/reset.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
