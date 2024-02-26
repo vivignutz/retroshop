fdeb9010d44089d64284613c5f61456017467dd6
vivignutz <vgnutzmann@gmail.com>
2024-02-26 22:39:21 +0100

    Added images at products and button to cards

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,29 +2,61 @@
 
 import React from "react";
 import styles from "../ProductCard/ProductCard.module.css";
-// import React, { useState, useEffect } from "react";
-// import { Spinner } from 'react-bootstrap';
-// import axios from "axios";
-// import Header from "../Header/Header";
-// import Footer from "../Footer/Footer";
+import { Button } from 'react-bootstrap';
+
 
 const ProductCard = ({ product }) => {
-
     return (
-        <div className={styles.container}>
-            <div className={styles.cardContainer}>
-                <img className={styles.productImage} src={product.image} alt={product.name} />
-                <div>
-                    <h1 className={styles.productTitle}>{product.name}</h1>
-                    <p className={styles.productDescription}>{product.description}</p>
-                    <p className={styles.productPrice}>Price: ${product.sale_price}</p>
-                </div>
-            </div>
+        <div className={styles.cardContainer}>
+            <img className={styles.productImage} src={product.image} alt={product.product_name} />
+            <div className={styles.cardBody}>
+                <h1 className={styles.productTitle}>{product.product_name}</h1>
+                <p className={styles.productDescription}>{product.description}</p>
+                <p className={styles.productCategory}>{product.category}</p>
+                <p className={styles.productMaterial}>{product.material}</p>
+                <p className={styles.productManufacturedYear}>{product.manufacturedYear}</p>
+                <p className={styles.productCondition}>{product.condition}</p>
+                <p className={styles.productOrigin}>{product.origin}</p>
+                <p className={styles.productCategory}>{product.category}</p>
+                <p className={`${styles.productPrice} card-text d-inline-block mr-3`}>Price: ${product.sale_price}</p>
+                <Button variant="success">Add to Cart</Button> 
+            </div>           
         </div>
     );
 }
 
 export default ProductCard;
+
+
+
+
+// const ProductCard = (params) => {
+//     const { product: product } = params;
+//     const { _id } = product
+//     const id = _id;
+
+//     return (
+//         <div className={styles.container}>
+//             <div className={styles.cardContainer}>
+//                 <img className={styles.productImage} src={product.image} alt={product.name} />
+//                 <div>
+//                     <h1 className={styles.productTitle}>{product.name}</h1>
+//                     <p className={styles.productDescription}>{product.description}</p>
+//                     <p className={styles.productCategory}>{product.category}</p>
+//                     <p className={styles.productMaterial}>{product.material}</p>
+//                     <p className={styles.productManufacturedYear}>{product.manufacturedYear}</p>
+//                     <p className={styles.productCondition}>{product.condition}</p>
+//                     <p className={styles.productOrigin}>{product.origin}</p>
+//                     <p className={styles.productOriginalPrice}>{product.originalPrice}</p>
+//                     <p className={styles.productCategory}>{product.category}</p>
+//                     <p className={styles.productPrice}>Price: ${product.sale_price}</p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default ProductCard;
 
     // const [product, setProduct] = useState(null);
 
--- src/pages/ProductList/Products.jsx
+++ src/pages/ProductList/Products.jsx
@@ -3,25 +3,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProductCard from "../../components/ProductCard/ProductCard.jsx";
-// import Categories from "../../components/Categories/Categories.jsx"
 import styles from "./Products.module.css"
 
-
 const Products = () => {
-  const [products, setProducts] = useState ([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetchData();
-  },[]);
+  }, []);
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/product`); 
-
-        localStorage.setItem('favoriteProducts', JSON.stringify(response.data));
-        const lsProducts = JSON.parse(localStorage.getItem('favoriteProducts'));
-        setProducts(lsProducts);
-      
+      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/product`);
+      const cloudinaryBaseUrl = 'https://res.cloudinary.com/dovykh024/image/upload/';
+      const productsWithCloudinaryUrls = response.data.map(product => ({
+        ...product,
+        imageUrl: `${cloudinaryBaseUrl}${product.cloudinaryImageId}.${product.imageExtension}`
+      }));
+      setProducts(productsWithCloudinaryUrls);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -30,25 +29,21 @@ const Products = () => {
   return (
     <>
       <div className={`container-fluid ${styles.productsContainer}`}>
-        <section className={`${ styles.container}mt-auto py.2`}>
-          <div className={`order-lg-1 text-start`}>
+        <section className={`${styles.container} mt-auto py-2`}>
+          <div className={`order-lg-1 text-start ${styles.textProducts}`}>
             <h3 className={styles.productsTitle}>All Products</h3>
             <br />
-            
-              {/* <Categories /> */}
-              <div className='product'>
-                <div className='container mt-5'>
-                  <div className="container">
-                      <div className='row'>
-                        {products.map((product) => (
-                          <div key={product._id} className='col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-3'>
-                            <ProductCard product={product} />
-                          </div>
-                        ))}
-                      </div>
+            <div className="product">
+            <div className="container mt-5">
+                <div className="row row-cols-1 row-cols-md-1 row-cols-lg-2 row-cols-xl-2 g-4">
+                  {products.map((product) => (
+                    <div key={product._id} className="col mb-4">
+                      <ProductCard product={product} />
                     </div>
-                  </div>
+                  ))}
                 </div>
+              </div>
+            </div>
           </div>
         </section>
       </div>
@@ -57,3 +52,57 @@ const Products = () => {
 };
 
 export default Products;
+
+
+
+
+// const Products = () => {
+//   const [products, setProducts] = useState ([]);
+
+//   useEffect(() => {
+//     fetchData();
+//   },[]);
+
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/product`); 
+
+//         localStorage.setItem('favoriteProducts', JSON.stringify(response.data));
+//         const lsProducts = JSON.parse(localStorage.getItem('favoriteProducts'));
+//         setProducts(lsProducts);
+      
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className={`container-fluid ${styles.productsContainer}`}>
+//         <section className={`${ styles.container}mt-auto py.2`}>
+//           <div className={`order-lg-1 text-start ${styles.textProducts}`}>
+//             <h3 className={styles.productsTitle}>All Products</h3>
+//             <br />
+
+//               {/* <Categories /> */}
+//               <div className='product'>
+//                 <div className='container mt-5'>
+//                   <div className="container">
+//                       <div className='row'>
+//                         {products.map((product) => (
+//                           <div key={product._id} className='col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-3'>
+//                             <ProductCard product={product} />
+//                           </div>
+//                         ))}
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//           </div>
+//         </section>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Products;
