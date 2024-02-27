be31ac6d2366bbffff91a96fee001ebad2d38971
vivignutz <vgnutzmann@gmail.com>
2024-02-27 10:11:29 +0100

    reinstalle node modules und updated react;router.com

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -7,21 +7,22 @@ import { Button } from 'react-bootstrap';
 
 const ProductCard = ({ product }) => {
     return (
-        <div className={styles.cardContainer}>
-            <img className={styles.productImage} src={product.image} alt={product.product_name} />
-            <div className={styles.cardBody}>
-                <h1 className={styles.productTitle}>{product.product_name}</h1>
-                <p className={styles.productDescription}>{product.description}</p>
-                <p className={styles.productCategory}>{product.category}</p>
-                <p className={styles.productMaterial}>{product.material}</p>
-                <p className={styles.productManufacturedYear}>{product.manufacturedYear}</p>
-                <p className={styles.productCondition}>{product.condition}</p>
-                <p className={styles.productOrigin}>{product.origin}</p>
-                <p className={styles.productCategory}>{product.category}</p>
-                <p className={`${styles.productPrice} card-text d-inline-block mr-3`}>Price: ${product.sale_price}</p>
-                <Button variant="success">Add to Cart</Button> 
-            </div>           
-        </div>
+        <a href={`/product/${product._id}`} className={styles.cardLink}>
+            <div className={styles.cardContainer}>
+                <img className={styles.productImage} src={product.image} alt={product.product_name} />
+                <div className={styles.cardBody}>
+                    <h1 className={styles.productTitle}>{product.product_name}</h1>
+                    <p className={styles.productDescription}>{product.description}</p>
+                    <p className={styles.productCategory}>{product.category}</p>
+                    <p className={styles.productMaterial}>{product.material}</p>
+                    <p className={styles.productManufacturedYear}>{product.manufacturedYear}</p>
+                    <p className={styles.productCondition}>{product.condition}</p>
+                    <p className={styles.productOrigin}>{product.origin}</p>
+                    <p className={`${styles.productPrice} card-text d-inline-block mr-3`}>Price: ${product.sale_price}</p>
+                    <Button className={styles.addToCartButton} variant="success">Add to Cart</Button> 
+                </div>           
+            </div>
+        </a>
     );
 }
 
